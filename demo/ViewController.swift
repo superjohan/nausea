9288220c79b1a8bfb0f3cd53b84e64dbcc4323ff
Johan Halin <johan@halin.me>
2019-03-24 11:49:43 +0200

    Improved autostart. The previous implementation didn't work.
    Don't know why, really.

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -72,7 +72,9 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
         
         self.view.addSubview(self.sceneView)
 
-        self.view.addSubview(self.startButton)
+        if !self.autostart {
+            self.view.addSubview(self.startButton)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -111,9 +113,7 @@ class ViewController: UIViewController, SCNSceneRendererDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        if (self.autostart) {
-            self.startButton.isUserInteractionEnabled = false
-            self.startButton.alpha = 0
+        if self.autostart {
             start()
         }
     }
